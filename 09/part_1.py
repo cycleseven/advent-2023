11cc90f8793ab0b268b27bcf79c0b430bbf35e9f
Owen Gillespie <owen@cy7.io>
2023-12-14 00:03:27 +0000

    Day 9

---
+++ 09/part_1.py
@@ -0,0 +1,17 @@
+import sys
+
+sum = 0
+
+for line in sys.stdin:
+    values = [[int(x) for x in line.split()]]
+
+    while not all(x == 0 for x in values[-1]):
+        values.append([b - a for a, b in zip(values[-1], values[-1][1:])])
+
+    predicted_value = 0
+    for row in reversed(values[:-1]):
+        predicted_value = predicted_value + row[-1]
+
+    sum += predicted_value
+
+print(sum)
